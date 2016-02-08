0feff22bed9fb010e90da6df65b032483e825d01
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2016-02-08 13:50:41 +0200

    rb_ua recovery service started

--- qcom/msm8916_64/msm8916_64.mk
+++ qcom/msm8916_64/msm8916_64.mk
@@ -182,9 +182,11 @@ PRODUCT_COPY_FILES += device/qcom/msm8916_64/EHANGAUTOINST.ISO:system/etc/EHANGA
 #added by shengweiguang for cd-rom iso copy end	 2015/10/27  end
 
 PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/rb_ua.conf:system/etc/rb_ua.conf
-#PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/rb_ua_r.conf:root/etc/rb_ua_r.conf
 PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/rb_ua:system/bin/rb_ua
 PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/rb_recovery.fstab:system/etc/rb_recovery.fstab
+PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/rb_ua:root/sbin/rb_ua
+#test
+PRODUCT_COPY_FILES += product_config/3rd/rb_fota_update/setup/test.txt:system/etc/test.txt
 
 PRODUCT_SUPPORTS_VERITY := true
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
